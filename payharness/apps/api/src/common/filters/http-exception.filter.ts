9766a5baa3d23f49586352548fca8aa345d7a9e4
Patrick Ongeche <daltonongeche@gmail.com>
2026-07-23 15:21:25 +0300

    Add Logger to HttpExceptionFilter

--- payharness/apps/api/src/common/filters/http-exception.filter.ts
+++ payharness/apps/api/src/common/filters/http-exception.filter.ts
@@ -1,4 +1,4 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
