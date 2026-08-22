1bb0cb486d73889fe1b31e17facc97ae9dee9a53
Patrick Ongeche <daltonongeche@gmail.com>
2026-08-22 18:08:44 +0300

    feat: register webhook delivery service

--- payharness/apps/api/src/webhooks/webhooks.module.ts
+++ payharness/apps/api/src/webhooks/webhooks.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuditLogsModule } from '../audit-logs/audit-logs.module';
 import { WebhooksController } from './webhooks.controller';
+import { WebhookDeliveryService } from './webhook-delivery.service';
 import { WebhooksService } from './webhooks.service';
 
 @Module({
   imports: [AuditLogsModule],
   controllers: [WebhooksController],
-  providers: [WebhooksService],
+  providers: [WebhooksService, WebhookDeliveryService],
+  exports: [WebhookDeliveryService],
 })
 export class WebhooksModule {}
