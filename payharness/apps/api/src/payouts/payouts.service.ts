e0ae3c76a16b7cfca3d41e10873b01f08301c7e0
Patrick Ongeche <daltononge@gmail.com>
2026-09-11 13:05:30 +0300

    fix: remove unused payout import

--- payharness/apps/api/src/payouts/payouts.service.ts
+++ payharness/apps/api/src/payouts/payouts.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Environment, Prisma, Provider } from '@prisma/client';
 import { randomUUID } from 'crypto';
 import { PrismaService } from '../common/prisma.service';
