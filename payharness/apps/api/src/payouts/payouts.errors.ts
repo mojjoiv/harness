f7cf174da4a5f9bc4be4a634fd249e01741e2dd5
Patrick Ongeche <daltononge@gmail.com>
2026-09-11 13:07:58 +0300

    chore: define payout errors

---
+++ payharness/apps/api/src/payouts/payouts.errors.ts
@@ -0,0 +1,5 @@
+export const PAYOUT_ERRORS = {
+  NOT_FOUND: 'Payout not found',
+  CREATION_FAILED: 'Payout could not be created',
+  IDEMPOTENCY_REQUIRED: 'Idempotency-Key header is required',
+} as const;
