8fc08c3673f0e224123aca14aac2ddb5b89c09a3
Patrick Ongeche <daltononge@gmail.com>
2026-09-11 13:14:44 +0300

    chore: remove unused payout errors

--- payharness/apps/api/src/payouts/payouts.errors.ts
+++
@@ -1,5 +0,0 @@
-export const PAYOUT_ERRORS = {
-  NOT_FOUND: 'Payout not found',
-  CREATION_FAILED: 'Payout could not be created',
-  IDEMPOTENCY_REQUIRED: 'Idempotency-Key header is required',
-} as const;
