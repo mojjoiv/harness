502804379721af4e210c10973a52c43de6d7e782
Patrick Ongeche <daltononge@gmail.com>
2026-09-12 16:17:44 +0300

    feat: expose M-Pesa payout callbacks

---
+++ payharness/apps/api/src/payouts/mpesa-payout-callback.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import {
+  MpesaPayoutCallbackResult,
+  MpesaPayoutCallbackService,
+} from './mpesa-payout-callback.service';
+
+@Controller('webhooks/provider/MPESA')
+export class MpesaPayoutCallbackController {
+  constructor(private readonly callbacks: MpesaPayoutCallbackService) {}
+
+  @Post(':merchantId/result')
+  result(
+    @Param('merchantId') merchantId: string,
+    @Body() body: MpesaPayoutCallbackResult,
+  ) {
+    return this.callbacks.handleResult(merchantId, body);
+  }
+
+  @Post(':merchantId/timeout')
+  timeout(
+    @Param('merchantId') merchantId: string,
+    @Body() body: MpesaPayoutCallbackResult,
+  ) {
+    return this.callbacks.handleTimeout(merchantId, body);
+  }
+}
